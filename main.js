47545b0a12a105a6d709717a577745ff93cb1c61
Christel Mogollon <christel.floresm@gmail.com>
2018-10-30 18:22:40 -0400

    Added files

---
+++ main.js
@@ -0,0 +1,14 @@
+// Hero Image text
+// var text = document.getElementById("home");
+// text.addEventListener("click", typeWriter);
+//
+// function typeWriter(){
+//   var text = "Munder Difflin";
+//   var speed = 50;
+//
+//   for (let i = 0; i < text.length;) {
+//     document.getElementById("home").innerHTML += text.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
